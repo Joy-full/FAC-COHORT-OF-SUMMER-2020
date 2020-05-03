6e893a1dcee45fd54ac8e4628672f8633495db39
Joy-full <dlwhdms0326@gmail.com>
2020-05-03 23:20:14 +0100

    Add dots-active and dots-hover

--- script1.js
+++ script1.js
@@ -6,8 +6,13 @@ function plusIndex(n){
   showImage(slideIndex += n);
 }
 
+function currentSlide(n) {
+  showImage(slideIndex = n);
+}
+
 function showImage(n){
   var slide = document.getElementsByClassName("slides")
+  var dots = document.getElementsByClassName("dots");
 
   if(n > slide.length) {slideIndex = 1};
   if(n < 1) {slideIndex = slide.length};
@@ -17,4 +22,10 @@ function showImage(n){
 
   };
   slide[slideIndex-1].style.display = "block";
+
+  for (var i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  };
+
+  dots[slideIndex-1].className += " active";
 }
